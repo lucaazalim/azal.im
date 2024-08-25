bd71c112ad9ba4587dbfb75ca9ead1fad45681e6
Luca Azalim <lucaazalim@gmail.com>
2024-08-25 19:56:23 -0300

    Removes automatic skill sorting.

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,7 +7,7 @@ type SkillsProps = {
 export default function Skills({skills}: SkillsProps) {
     return (
         <div className="flex items-center flex-wrap gap-2">
-            {skills.sort((a, b) => a.localeCompare(b)).map((skill, index) =>
+            {skills.map((skill, index) =>
                 <div key={index} className="flex items-center gap-1">
                     <Badge>{skill}</Badge>
                 </div>
