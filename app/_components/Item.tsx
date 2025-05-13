4501af936de62af97c36f9d78269be805d02358f
Luca Azalim <lucaazalim@gmail.com>
2025-05-13 19:22:39 -0300

    Improving responsivity of items in home page.

--- app/_components/Item.tsx
+++ app/_components/Item.tsx
@@ -36,7 +36,12 @@ export default function Item({
         <div className="relative group/container lg:hover:scale-[101%] lg:hover:bg-white/5 lg:hover:bg-gradient-to-br from-white/10 to-white/1 border-t-[1px] lg:hover:border-t-white/20 lg:hover:border-b-black/50 lg:border-transparent lg:hover:!opacity-100 lg:group-hover/section:opacity-50 rounded-lg p-8 transition-all border border-white/10">
           <div className="grid md:grid-cols-[0.3fr,0.7fr] gap-5">
             <div className="flex flex-col gap-4">
-              <p className="mt-1 text-xs font-semibold text-wrap">{date}</p>
+              <div className="flex items-center gap-3">
+                <span className="mt-1 text-xs font-semibold text-wrap">
+                  {date}
+                </span>
+                {badge && <Badge>{badge}</Badge>}
+              </div>
               {image && (
                 <Image
                   src={image}
@@ -54,18 +59,11 @@ export default function Item({
             <div className="space-y-4">
               <div className="flex justify-between gap-2">
                 <div className="flex items-center space-x-2">
-                  {badge && <Badge>{badge}</Badge>}
-                  <div>
+                  <div className="flex flex-col gap-2">
                     <div className="relative flex w-fit flex-row items-center gap-2">
-                      <p>{title}</p>
+                      <span className="font-semibold">{title}</span>
                       {link && (
-                        <ArrowUpRight
-                          className="
-                                                absolute size-5 transition-all
-                                                top-0 -right-6 lg:top-2 lg:-right-4
-                                                lg:group-hover/container:top-0 lg:group-hover/container:-right-6
-                                                lg:opacity-0 group-hover/container:opacity-100"
-                        />
+                        <ArrowUpRight className="hidden lg:block absolute size-5 transition-all top-2 -right-4 group-hover/container:top-0 group-hover/container:-right-6 opacity-0 group-hover/container:opacity-100" />
                       )}
                     </div>
                     <p className="text-sm text-foreground-muted">{subtitle}</p>
