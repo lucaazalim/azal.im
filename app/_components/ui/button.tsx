2877033ad6fd87fca4228c31b6a68633b1783ebe
Luca Azalim <lucaazalim@gmail.com>
2025-05-30 11:09:10 -0300

    Fix responsivity of CV page language buttons.

--- app/_components/ui/button.tsx
+++ app/_components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        lg: "h-12 rounded-md px-6 has-[>svg]:px-4 text-md",
         icon: "size-9",
       },
     },
--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -3,6 +3,7 @@
 import { Language } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { Button } from "../_components/ui/button";
 
 const cv: {
   id: Language;
@@ -29,37 +30,23 @@ export default function CV() {
 
   return (
     <div className="mx-auto flex max-w-xl flex-col items-center space-y-10 p-10">
-      <fieldset className="grid w-full grid-cols-2 gap-5 text-center text-xl">
-        <legend className="sr-only">Switch language</legend>
-        {Object.values(cv).map(({ id, name, label }) => {
-          const checked = language === id;
-
-          return (
-            <div
-              key={id}
-              className={cn(
-                "bg-accent cursor-pointer space-x-3 rounded-lg border-2 p-4",
-                checked &&
-                  "outline-primary bg-primary/10 border-primary/20 outline-2 outline-offset-2",
-              )}
-              onClick={() => setLanguage(id)}
-            >
-              <input
-                className="sr-only cursor-pointer"
-                type="radio"
-                id={id}
-                name={name}
-                value={id}
-                checked={checked}
-                readOnly
-              />
-              <label htmlFor={id} className="cursor-pointer">
-                {label}
-              </label>
-            </div>
-          );
-        })}
-      </fieldset>
+      <div className="grid w-full grid-cols-2">
+        {cv.map((c) => (
+          <Button
+            key={c.id}
+            variant="outline"
+            size="lg"
+            className={cn("", {
+              "rounded-r-none": c.id === "en",
+              "rounded-l-none": c.id === "pt",
+              "opacity-50": language !== c.id,
+            })}
+            onClick={() => setLanguage(c.id)}
+          >
+            {c.label}
+          </Button>
+        ))}
+      </div>
       <iframe
         className="animate-in slide-in-from-bottom-10 fade-in h-[75vh] w-[80vw] grow rounded-lg duration-1000"
         src={cv.find((c) => c.id === language)?.url}
