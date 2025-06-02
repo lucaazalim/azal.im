77d6ea4e62572593cf192b341731c91c91d41dc0
Luca Azalim <lucaazalim@gmail.com>
2025-06-02 18:00:17 -0300

    Fix page header padding.

--- app/_components/header/PageHeader.tsx
+++ app/_components/header/PageHeader.tsx
@@ -14,7 +14,7 @@ export default function PageHeader({
   return (
     <header
       className={cn(
-        "mx-auto flex flex-col items-center py-8 text-center",
+        "mx-auto flex flex-col items-center p-8 text-center",
         animation && "animate-in fade-in duration-500 ease-in",
         className,
       )}
