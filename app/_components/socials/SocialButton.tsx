75dcd2d3905ca18705a80efb829634f575a6503b
Luca Azalim <lucaazalim@gmail.com>
2025-06-09 17:31:41 -0300

    Add target="_blank" to social links.

--- app/_components/socials/SocialButton.tsx
+++ app/_components/socials/SocialButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function SocialButton({ icon: Icon, href, ...props }: Props) {
   return (
-    <a href={href} {...props}>
+    <a href={href} target="_blank" {...props}>
       <div className="bg-accent hover:border-foreground/20 group flex aspect-square items-center justify-center border-1 p-2 transition-colors duration-300 ease-in-out">
         <Icon className="text-foreground size-5" />
       </div>
