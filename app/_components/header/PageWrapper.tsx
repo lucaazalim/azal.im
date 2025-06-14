04583e2484cd9d22507fd6f5f94abbf044572b97
Luca Azalim <lucaazalim@gmail.com>
2025-06-14 00:08:26 -0300

    Fix page wrapper padding.

--- app/_components/header/PageWrapper.tsx
+++ app/_components/header/PageWrapper.tsx
@@ -5,12 +5,6 @@ type Props = HTMLAttributes<HTMLDivElement>;
 
 export default function PageWrapper({ className, ...props }: Props) {
   return (
-    <div
-      className={cn(
-        className,
-        "mx-auto space-y-10 px-5 pb-5 md:px-10 md:pb-10",
-      )}
-      {...props}
-    />
+    <div className={cn(className, "mx-auto space-y-10 px-5 pb-5")} {...props} />
   );
 }
