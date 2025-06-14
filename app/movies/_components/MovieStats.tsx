4696766d5fb009c93b543980dbb802aa5560ce3b
Luca Azalim <lucaazalim@gmail.com>
2025-06-14 00:21:55 -0300

    Fix place-self-center does not work properly in Safari.

--- app/movies/_components/MovieStats.tsx
+++ app/movies/_components/MovieStats.tsx
@@ -3,27 +3,29 @@ import { totalMovies, totalRuntime } from "@/lib/movies/movies";
 
 export default function MovieStats() {
   return (
-    <div className="border-px grid grid-cols-2 gap-10 place-self-center border-x px-5">
-      <span className="flex flex-col items-center">
-        <CountUp
-          className="text-xl font-semibold"
-          from={0}
-          to={totalRuntime / 60}
-          separator=","
-          duration={1}
-        />
-        <span className="font-mono uppercase opacity-50">hours</span>
-      </span>
-      <span className="flex flex-col items-center">
-        <CountUp
-          className="text-xl font-semibold"
-          from={0}
-          to={totalMovies}
-          separator=","
-          duration={1}
-        />
-        <span className="font-mono uppercase opacity-50">titles</span>
-      </span>
+    <div className="flex items-center justify-center">
+      <div className="border-px grid grid-cols-2 gap-10 border-x px-5">
+        <span className="flex flex-col items-center">
+          <CountUp
+            className="text-xl font-semibold"
+            from={0}
+            to={totalRuntime / 60}
+            separator=","
+            duration={1}
+          />
+          <span className="font-mono uppercase opacity-50">hours</span>
+        </span>
+        <span className="flex flex-col items-center">
+          <CountUp
+            className="text-xl font-semibold"
+            from={0}
+            to={totalMovies}
+            separator=","
+            duration={1}
+          />
+          <span className="font-mono uppercase opacity-50">titles</span>
+        </span>
+      </div>
     </div>
   );
 }
